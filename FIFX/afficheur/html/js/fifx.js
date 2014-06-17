12c4d39c0d409edc21ea946368cf115a39dc815b
Alexandre Girard <git@alexgirard.com>
2014-06-17 21:07:30 +0200

    add minimum setup

---
+++ FIFX/afficheur/html/js/fifx.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  var refreshSlides = function() {
+    $.getJSON( "gifList.json", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<img src='" + val + "' />" );
+      });
+      $('#slides').html( items.join( "" );
+      slider.start();
+    });
+  }
+
+  var slider = $('#slides').slidesjs({
+    width: 940,
+    height: 528,
+    auto: false,
+    callback: {
+      loaded: function(number) {
+        refreshSlides();
+      },
+      complete: funciton(number) {
+        refreshSlides();
+      }
+  });
+});
